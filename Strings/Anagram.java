cd6153b85936940d1c3de50a5a49480dfea50791
Yash Israni <yashisrani52@gmail.com>
2024-12-15 11:00:56 +0530

    Strings solutions

---
+++ Strings/Anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    // Function is to check whether two strings are anagram of each other or not.
+    public static boolean areAnagrams(String s1, String s2) {
+
+        // Your code here
+         int[] arr = new int[26];
+        for(char c : s1.toCharArray()){
+            ++arr[c - 'a'];
+        }
+        
+        for(char c : s2.toCharArray()){
+            --arr[c - 'a'];
+        }
+        
+        for(int freq : arr){
+            if(freq != 0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
